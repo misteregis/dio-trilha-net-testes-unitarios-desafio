f314a0fb0c48e6f2269708ee8914ef2c3ea03aa4
misteregis <misteregis@gmail.com>
2023-10-23 19:42:57 -0300

    Atualizado packs e alterado métodos para estático

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -2,30 +2,30 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesLista
     {
-        public List<int> RemoverNumerosNegativos(List<int> lista)
+        public static List<int> RemoverNumerosNegativos(List<int> lista)
         {
             var listaSemNegativos = lista.Where(x => x > 0);
             return listaSemNegativos.ToList();
         }
 
-        public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
+        public static bool ListaContemDeterminadoNumero(List<int> lista, int numero)
         {
             var contem = lista.Contains(numero);
             return contem;
         }
 
-        public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
+        public static List<int> MultiplicarNumerosLista(List<int> lista, int numero)
         {
             var listaMultiplicada = lista.Select(x => x * numero).ToList();
             return listaMultiplicada;
         }
 
-        public int RetornarMaiorNumeroLista(List<int> lista)
+        public static int RetornarMaiorNumeroLista(List<int> lista)
         {
             return lista.Max();
         }
 
-        public int RetornarMenorNumeroLista(List<int> lista)
+        public static int RetornarMenorNumeroLista(List<int> lista)
         {
             return lista.Min();
         }
--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -2,19 +2,19 @@ namespace TestesUnitarios.Desafio.Console.Services
 {
     public class ValidacoesString
     {
-        public int RetornarQuantidadeCaracteres(string texto)
+        public static int RetornarQuantidadeCaracteres(string texto)
         {
             var numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
 
-        public bool ContemCaractere(string texto, string textoProcurado)
+        public static bool ContemCaractere(string texto, string textoProcurado)
         {
             var contem = texto.Contains(textoProcurado);
             return contem;
         }
 
-        public bool TextoTerminaCom(string texto, string textoProcurado)
+        public static bool TextoTerminaCom(string texto, string textoProcurado)
         {
             var termina = texto.EndsWith(textoProcurado);
             return termina;
